50fb26fed4f015921e365e888591180caca992c5
Mike L <mike.leeder@gmail.com>
2024-05-10 15:23:46 -0700

    search endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import Body, FastAPI, HTTPException
 from pymongo import MongoClient
 from models.monster import Monster
 from typing import List
@@ -19,6 +19,14 @@ app = FastAPI()
 @app.get('/monsters', response_model=List[Monster])
 def get_all_monsters():
     return list(collection.find({}, {'_id': False}))
+
+@app.get("/monsters/search", response_model=List[Monster])
+def search_monsters(**filters: dict):
+    query = {}
+    for k, v in filters.items():
+        if k in Monster.__annotations__:
+            query[k] = v
+    return list(collection.find(query, {'_id': False}))
 
 @app.get('/monsters/{name}', response_model=Monster)
 def get_monster_by_name(name: str):
